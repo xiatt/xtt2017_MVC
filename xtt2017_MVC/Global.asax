aa77c31f9898451b427eb52918160050591326df
Administrator <Administrator@6-206-A19>
2019-05-16 19:18:27 +0800

    添加项目文件。

---
+++ xtt2017_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="xtt2017_MVC.MvcApplication" Language="C#" %>
---
+++ xtt2017_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(xtt2017_MVC.Startup))]
+namespace xtt2017_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
